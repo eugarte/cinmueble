3b9874bc386606ba9939b09b54e89251c832e7e5
eugarte <zugarte@outlook.com>
2019-09-01 21:17:08 -0500

    Se actualizó lo cambios de acuerdo al nuevo proceso

--- assets/scripts/bienes.js
+++ assets/scripts/bienes.js
@@ -1,77 +1,65 @@
 function ReiniciarValores()
 {
-  document.getElementById("nVenta").value = "";
-  document.getElementById("nAlquiler").value = "";
-  document.getElementById("nMetros").value = "";
-  document.getElementById("cCelular").value = "";
-  document.getElementById("cDireccion").value = "";
-  document.getElementById("nLatitud").value = "";
-  document.getElementById("nLongitud").value = "";
-  document.getElementById("cbbTipo").value = "";
   document.getElementById("cbbMedio").value = "";
-  document.getElementById("cbbSecEco").value = "";
   document.getElementById("cbbDistrito").value = "";
-  document.getElementById("cbbCenCom").value = "";
-  
-  var radios = document.getElementsByName('options');
-  for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-          // do whatever you want with the checked radio
-          radios[i].checked = false;
-          break;
-      }
-  }  
-  document.getElementById("cDetalle").value = "";
+  document.getElementById("nMetros").value = "";
+  document.getElementById("nAlquiler").value = "";
+  document.getElementById("nVenta").value = "";
+  document.getElementById("cCelular").value = "";
+  document.getElementById("cReferencia").value = "";
+  document.getElementById("dFecVis").value = "";
+  document.getElementById("cAgua").checked = false;
+  document.getElementById("cLuz").checked = false;
+  document.getElementById("cbbTipoTrato").value = "";
+  document.getElementById("cbbTipoNegocio").value = "";
+  document.getElementById("cComentario").value = "";
+  document.getElementById("cAntiguedad").value = "";   
+
 }
 function RegistrarBienes() {
   document.getElementById("btnBien").disabled = true;
   var loBien = {};  
-  var lnVenta = document.getElementById("nVenta").value;
-  var lnAlquiler = document.getElementById("nAlquiler").value;
-  var lnMetros = document.getElementById("nMetros").value;
-  var lcCelular = document.getElementById("cCelular").value;
-  var lcDireccion = document.getElementById("cDireccion").value;
-  var lnLatitud = document.getElementById("nLatitud").value;
-  var lnLongitud = "";//document.getElementById("nLongitud").value;
-  var e = document.getElementById("cbbTipo");
-  var lcTipo = e.options[e.selectedIndex].value; 
-  e = document.getElementById("cbbMedio");
-  var lcMedio = e.options[e.selectedIndex].value; 
-  e = document.getElementById("cbbSecEco");
-  var lcSecEco = e.options[e.selectedIndex].value; 
-  e = document.getElementById("cbbDistrito");
-  var lcDistrito = e.options[e.selectedIndex].value; 
-  e = document.getElementById("cbbCenCom");
-  var lcCenCom = e.options[e.selectedIndex].value; 
+  var lnVenta = document.getElementById("nVenta").value;//
+  var lnAlquiler = document.getElementById("nAlquiler").value;//
+  var lnMetros = document.getElementById("nMetros").value;//
+  var lcCelular = document.getElementById("cCelular").value;//
+  var lcReferencia = document.getElementById("cReferencia").value;//
+  var ldFecVis = document.getElementById("dFecVis").value;//
+  var lcComentario = document.getElementById("cComentario").value;//
+  var lcAntiguedad = document.getElementById("cAntiguedad").value;//
 
-  var radios = document.getElementsByName('options');
-  for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-          // do whatever you want with the checked radio
-          var lcImportancia = radios[i].value;
-          break;
-      }
-  }  
-  var lcDetalle = document.getElementById("cDetalle").value;
+  var e = document.getElementById("cbbMedio");//
+  var lcMedio = e.options[e.selectedIndex].value; 
+  e = document.getElementById("cbbDistrito");//
+  var lcDistrito = e.options[e.selectedIndex].value; 
+  e = document.getElementById("cbbTipoTrato");//
+  var lcTipoTrato = e.options[e.selectedIndex].value; 
+  e = document.getElementById("cbbTipoNegocio");//
+  var lcTipoNegocio = e.options[e.selectedIndex].value; 
+
+  var lcAgua = document.getElementById("cAgua").checked == true ? "1" : "2";//
+  var lcLuz = document.getElementById("cLuz").checked == true ? "1" : "2";//
+
   var today = new Date();
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   
+  loBien.cMedio             =lcMedio;
+  loBien.cDistrito          =lcDistrito;
+  loBien.nMetros            =lnMetros;
+  loBien.nPrecioRenta       =lnAlquiler;
+  loBien.nPrecioCompra      =lnVenta;
+  loBien.cCelular           =lcCelular;
+  loBien.dFecha             =dateToYMD(new Date()) +" "+ time;//"2019-08-08 00:00:00";
+  loBien.cReferencia        =lcReferencia;
+  loBien.dFecRegLlamada     =dateToYMD(new Date()) +" "+ time;//"2019-08-08 00:00:00";;
+  loBien.dFecVisita         =ldFecVis;
+  loBien.cAgua              =lcAgua;
+  loBien.cLuz               =lcLuz;
+  loBien.cTrato             =lcTipoTrato;
+  loBien.cTipoNegocio       =lcTipoNegocio;
+  loBien.cComentarioLlamada =lcComentario;
+  loBien.cAntiguedad        =lcAntiguedad;
 
-  loBien.cTipo = lcTipo;//
-  loBien.cMedio = lcMedio;//
-  loBien.cSectorEco = lcSecEco;//
-  loBien.cImportancia = lcImportancia;//
-  loBien.cDetalle = lcDetalle;//
-  loBien.cDireccion = lcDireccion;//
-  loBien.cDistrito = lcDistrito;//
-  loBien.cCentroComercial = lcCenCom;//
-  loBien.nMetros = lnMetros;//
-  loBien.nPrecioRenta = lnVenta;//
-  loBien.nPrecioCompra = lnAlquiler;//
-  loBien.cLatitud = lnLatitud;//
-  //loBien.cLongitud = lnLongitud;//
-  loBien.cCelular = lcCelular;
-  loBien.dFecha =  dateToYMD(new Date()) +" "+ time;//"2019-08-08 00:00:00";
   if (ValidarCampos(loBien) == "1")
   {
    Registrar(loBien);  
@@ -87,75 +75,72 @@ function ValidarCampos(loBien)
   var llOk = "1";
   var lcTitulo = "¡Revisa!"
   var lcTipo = "3";
-  if(loBien.cTipo == "")
+              
+           
+  if(loBien.cTrato == "")
   {
-    Mensajes(lcTipo, lcTitulo, "Revisa Tipo");
+    Mensajes(lcTipo, lcTitulo, "Trato");
     llOk = "0";
   }
-  if(loBien.cMedio == "")
+  if(loBien.cMedio == "")//
   {
     Mensajes(lcTipo, lcTitulo, "Revisa Medio");
     llOk = "0";
   }
-  if(loBien.cSectorEco == "")
+  if(loBien.cLuz == "")//
   {
-    Mensajes(lcTipo, lcTitulo, "Sector económico");  
+    Mensajes(lcTipo, lcTitulo, "Luz");  
     llOk = "0";
   }
-  if(loBien.cDistrito == "")
+  if(loBien.cDistrito == "")//
   {
      Mensajes(lcTipo, lcTitulo, "Sector distrito");  
      llOk = "0";
   } 
-  if(loBien.cCentroComercial == "")
+  if(loBien.cAgua == "")//
   {
-    Mensajes(lcTipo, lcTitulo, "Centro comercial");    
+    Mensajes(lcTipo, lcTitulo, "Agua");    
+    llOk = "0";
+  }  
+  if(loBien.cTipoNegocio == "")//
+  {
+    Mensajes(lcTipo, lcTitulo, "Tipo Negocio");
     llOk = "0";
   }
-  if(loBien.cImportancia == null)
+  if(loBien.cComentarioLlamada == "")//
   {
-    Mensajes(lcTipo, lcTitulo, "Importancia"); 
+    Mensajes(lcTipo, lcTitulo, "Comentario Llamada");
     llOk = "0";
   }
-  if(loBien.cDetalle == "")
-  {
-    Mensajes(lcTipo, lcTitulo, "Detalle");
-    llOk = "0";
-  }
-  if(loBien.cDireccion == "")
-  {
-    Mensajes(lcTipo, lcTitulo, "Dirección");
-    llOk = "0";
-  }
-  if(loBien.nMetros == "")
+  if(loBien.nMetros == "")//
   {
     Mensajes(lcTipo, lcTitulo, "Metros");
     llOk = "0";
   }
-  if(loBien.nPrecioRenta == "")
+  if(loBien.nPrecioRenta == "")//
   {
     Mensajes(lcTipo, lcTitulo, "Precio Renta");
     llOk = "0";
   }
-  if(loBien.nPrecioCompra == "")
+  if(loBien.nPrecioCompra == "")//
   {
     Mensajes(lcTipo, lcTitulo, "Precio Compra");  
     llOk = "0";
   }
-  if(loBien.cLatitud == "")
+  if(loBien.cAntiguedad == "")//
   {
-    Mensajes(lcTipo, lcTitulo, "Latitud");  
+    Mensajes(lcTipo, lcTitulo, "Antiguedad");  
     llOk = "0";
   }
-  if(loBien.cLongitud == "")
+  if(loBien.cReferencia == "")//
   {
-    Mensajes(lcTipo, lcTitulo, "Longitud"); 
-    llOk = "0"; 
+    Mensajes(lcTipo, lcTitulo, "Referencia");  
+    llOk = "0";
   }
-  if(loBien.cCelular == "")
+  if(loBien.dFecVisita == "")//
   {
-    Mensajes(lcTipo, lcTitulo, "Celular"); 
-    llOk = "0";   
+    Mensajes(lcTipo, lcTitulo, "Fecha Visita");  
+    llOk = "0";
   }
   return llOk;
   
